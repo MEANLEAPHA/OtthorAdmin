9d605450fa8aacb0a3f5269022456046c2a218f7
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-03-02 15:16:50 +0700

    need to step out two time

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,9 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
 
