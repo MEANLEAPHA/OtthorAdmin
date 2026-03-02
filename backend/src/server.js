12c1eedf9ff80556a4bb18af987a784c014b99e0
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-03-02 16:21:47 +0700

    fahh

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 
