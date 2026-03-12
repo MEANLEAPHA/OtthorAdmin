fd3bb4108037edcc96959c0601dfe123b3dedada
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-03-12 11:29:49 +0700

    hope it cuase by the server side

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,52 @@
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const path = require('path');
+
+// app.use(cors({
+//   origin: "https://otthoradmin-frontend.onrender.com",
+//   methods: ["GET","POST","PUT","DELETE"],
+//   allowedHeaders: ["Content-Type","Authorization"]
+// }));
+// app.use(express.json());
+
+
+// app.use(express.static(path.join(__dirname, "index.html")));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+// const {Authentication} = require("./routes/Authentication/authenticationRoute");
+// Authentication(app);
+
+// // Start Server
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log(`🚀 Server running at http://localhost:${port}`);
+// });
+
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
-const path = require('path');
 
 app.use(cors({
-  origin: "https://otthoradmin-frontend.onrender.com",
-  methods: ["GET","POST","PUT","DELETE"],
-  allowedHeaders: ["Content-Type","Authorization"]
+  origin: "https://otthoradmin-frontend.onrender.com"
 }));
+
 app.use(express.json());
 
+const { Authentication } = require("./routes/Authentication/authenticationRoute");
 
-app.use(express.static(path.join(__dirname, "index.html")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-const {Authentication} = require("./routes/Authentication/authenticationRoute");
 Authentication(app);
 
-// Start Server
+app.get("/", (req, res) => {
+  res.send("API Server Running");
+});
+
 const port = 3000;
+
 app.listen(port, () => {
-    console.log(`🚀 Server running at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
