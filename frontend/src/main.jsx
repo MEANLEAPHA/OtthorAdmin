a7e8183cbf0c231b823e71a483aa54bd14e3993c
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-03-13 15:39:07 +0700

    wrap page with contenxtProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,15 @@ import Page from './page';
 // import {GoogleOAuthProvider} from '@react-oauth/google';
 const main = document.getElementById('root');
 const root = createRoot(main);
-
+import ContextProvider from "./assets/context/context";
 // const CLIENT_ID = "769261651394-b80k5kn379hm5cbrai2auvj9r8jed9lf.apps.googleusercontent.com";
 root.render(
   <>
     <StrictMode>
+      <ContextProvider>
       {/* <GoogleOAuthProvider clientId={CLIENT_ID}> */}
           <Page />
+      </ContextProvider>
       {/* </GoogleOAuthProvider> */}
     </StrictMode>
   </>
